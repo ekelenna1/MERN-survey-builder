7e121bc49d5e3ff79db2289c800234a394fb1872
lcoll813 <122918140+lcoll813@users.noreply.github.com>
2025-11-28 21:28:17 -0700

    set up express server & cors config

---
+++ server/index.js
@@ -0,0 +1,18 @@
+require('dotenv').config();
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+
+const app = express();
+const PORT = process.env.PORT || 5001;
+
+app.use(cors());
+app.use(express.json());
+
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => console.log('DB Connected'))
+  .catch(err => console.error('DB Connection Error:', err));
+
+app.get('/', (req, res) => res.send('API Running'));
+
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
