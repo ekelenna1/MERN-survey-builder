6be6268428e7bfc5113ec2358e2653ea36a46ab3
lcoll813 <122918140+lcoll813@users.noreply.github.com>
2025-11-29 13:41:12 -0700

    add mongoose poll schema

---
+++ server/models/Poll.js
@@ -0,0 +1,28 @@
+const mongoose = require('mongoose');
+
+const PollSchema = new mongoose.Schema({
+  title: { type: String, required: true },
+  creator: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+    required: true
+  },
+  questions: [{
+    text: { type: String, required: true },
+    type: {
+      type: String,
+      required: true,
+      enum: ['multiple-choice', 'checkbox', 'text', 'scale', 'ranked']
+    },
+    options: [String]
+  }],
+  votes: [{
+    submittedAt: { type: Date, default: Date.now },
+    answers: [{
+      questionIndex: Number,
+      response: mongoose.Schema.Types.Mixed
+    }]
+  }]
+}, { timestamps: true });
+
+module.exports = mongoose.model('Poll', PollSchema);
