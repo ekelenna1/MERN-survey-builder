5da951f8e6b0538f2f38f8403d0bde1e0dd41c70
lcoll813 <122918140+lcoll813@users.noreply.github.com>
2025-12-01 12:23:26 -0600

    add post polls.js route to save new polls

--- server/routes/polls.js
+++ server/routes/polls.js
@@ -10,4 +10,25 @@ router.get('/mine', auth, (req, res) => {
     .catch(err => res.status(500).send('Server Error'));
 });
 
+router.post('/', auth, (req, res) => {
+    const { title, questions } = req.body;
+  
+    if (!title || !questions || questions.length === 0) {
+      return res.status(400).json({ msg: 'Please include a title and at least one question.' });
+    }
+  
+    const newPoll = new Poll({
+      title,
+      questions,
+      creator: req.user.id
+    });
+  
+    newPoll.save()
+      .then(poll => res.json(poll))
+      .catch(err => {
+        console.error(err.message);
+        res.status(500).send('Server Error');
+      });
+  });
+
 module.exports = router;
